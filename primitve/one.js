7204180825b5c7240b5c8335e1e94661c9a49c6d
abdul7amaan7 <amaan12171@gmail.com>
2026-08-23 21:48:13 +0530

    primitve types

---
+++ primitve/one.js
@@ -0,0 +1,8 @@
+"use strict";
+let adrakname = "abdul";
+let age = 34;
+let male = true;
+let salary = 876543243n;
+let uni = Symbol("adrak");
+let no = undefined;
+let yes = null;
---
+++ primitve/one.ts
@@ -0,0 +1,7 @@
+let adrakname : string = "abdul" 
+let age : number = 34
+let male : boolean = true
+let salary : bigint = 876543243n
+let uni : symbol = Symbol("adrak")
+let no : undefined = undefined
+let yes : null = null
